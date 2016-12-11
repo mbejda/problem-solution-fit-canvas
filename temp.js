ba3f2a739121a9f7f897cb7095485c005009862a
Milos Bejda <mbejda@live.com>
2016-12-10 21:13:12 -0500

    init

--- temp.js
+++
@@ -1,1 +0,0 @@
-db = db.getSiblingDB('annuals'); db.prop.find({'geoJson':{'$exists':false}}).forEach(function (e) {e.geoJson = { type: 'Point', coordinates : [e.situslongitude,e.situslatitude]};db.prop.save(e)})
